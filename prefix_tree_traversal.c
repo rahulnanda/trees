f159844e45a15394266f41d728c1893c60e3b842
Rahul Nanda <rahulnanda@users.noreply.github.com>
2017-01-20 23:17:23 -0800

    Create prefix_tree_traversal.c

---
+++ prefix_tree_traversal.c
@@ -0,0 +1,12 @@
+/* Traverse a prefix tree and print out all subnets which match
+ * the input subnet and mask. 
+ */
+ 
+ void prefix_tree_traversal(node* root, int addr, int mask) {
+    
+    if (root == NULL) {
+        return;
+    }
+    
+}
+        
