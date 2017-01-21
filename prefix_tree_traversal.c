27f80d866a00b795c39a6e5b8e3551a3befa5629
Rahul Nanda <rahulnanda@users.noreply.github.com>
2017-01-21 00:08:05 -0800

    Add traversal code

--- prefix_tree_traversal.c
+++ prefix_tree_traversal.c
@@ -2,11 +2,22 @@
  * the input subnet and mask. 
  */
  
- void prefix_tree_traversal(node* root, int addr, int mask) {
+ void prefix_tree_traversal(node* root, int val, int mask, int addr) {
     
     if (root == NULL) {
         return;
     }
+    
+    if (root->valid) {
+        printf("%d\n", addr);
+    }
+    
+    int bit = val & (1<<31);
+     
+     
+    if (val & (1<<31) == 0) {
+        
+        prefix_tree_traversal(root->left, val<<1, mask<<1, 
     
 }
         
