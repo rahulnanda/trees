24679e76172acafc6e79f59e2f22fd635a2e2217
Rahul Nanda <rahulnanda@users.noreply.github.com>
2017-01-22 14:02:29 -0800

    Create square_root.c

---
+++ square_root.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+float binary_search(float val, float low, float high){
+	float mid;
+	
+	mid = (low+high)/2;
+
+	if (fabs(val - mid*mid) > 0.0001) {
+		if (val > mid*mid) {
+			binary_search(val, mid, high);
+		} else {
+			binary_search(val, low, mid);
+		}
+	} else {
+		return mid;
+	}
+}
+
+float square_root(int val){
+	return binary_search((float)val, 0.0, (float)val);
+}
+
+int main(void) {
+	// your code goes here
+	printf("2: %f\n", square_root(2));
+	printf("3: %f\n", square_root(3));
+	printf("4: %f\n", square_root(4));
+	printf("9: %f\n", square_root(9));
+	printf("17: %f\n", square_root(17));
+	printf("25: %f\n", square_root(25));
+	printf("47: %f\n", square_root(47));
+	return 0;
+}
+
+
+
